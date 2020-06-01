2f7814d89ec42021f999add011f827b94ab46233
alexander-braun <alexanderbraun@freenet.de>
2020-06-01 23:03:52 +0200

    started % function

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,55 @@ import React, { MouseEvent, useState } from 'react';
 import './style/styles.css';
 import { Display } from './components/Display'
 
+function calc(n1: number, n2: number, operator: String): number {
+  if(operator === '+') return n1 + n2
+  if(operator === '-') return n1 - n2
+  if(operator === '/') return n1 / n2
+  else return n1 * n2
+}
+
+function calculatePercentage(chain: [number, string, number]): number {
+  const op: string = chain[1]
+  const n0: number = chain[0]
+  const n1: number = chain[2]
+
+  if(op[1] === '*' || op[1] === '/') {
+    if(op === '*') {
+      return n0 * n0/100*n1
+    } else {
+      return n0 / n0/100*n1
+    }
+  } else {
+    if(op === '-') {
+      return n0 - (n0 / 100 * n1)
+    } else {
+      return n0 + (n0 / 100 * n1)
+    }
+  }
+}
+
+function getCalcElements(chain: String[]) {
+
+  const positions: number[] = []
+  for(let i = 0; i < chain.length; i++) {
+    chain[i] === '%' && positions.push(i)
+  }
+
+  const elementRanges: number[][] = []
+  for(const element of positions) {
+    elementRanges.push([element - 3, element])
+  }
+
+  const newChain: any[] = []
+  for(let i = 0; i < chain.length; i++) {
+    
+  }
+}
+
 export default function App(): JSX.Element {
 
   const [chain, updateChain] = useState<String[]>([])
-  const [result, setResult] = useState<Number>(0)
+  const [result, setResult] = useState<number>(0)
 
   const handleClick = (e: MouseEvent) => {
     const target = e.currentTarget as HTMLButtonElement;
@@ -59,17 +104,14 @@ export default function App(): JSX.Element {
 
         // Case: There is a percentage sign in the chain
         if(hasPercent) {
-          let tempResult: Number = result
-          for(let i = 0; i < chain.length; i++) {
-            // Case: Chainelement is not a number
-            if(isNaN(+chain[i])) {
-              console.log(chain[i])
-            }
-          }
+          let tempResult: number = result
+          let currentOperator: String = ''
+
+
 
         } else {
           // eslint-disable-next-line no-eval
-          const tempResult: Number = chain.length !== 0 ? eval(chain.join('')) : 0
+          const tempResult: number = chain.length !== 0 ? eval(chain.join('')) : 0
           setResult(tempResult)
           updateChain([tempResult.toString()])
           return
