72a0529b89f157b00261b66bc51cfbf807743d4f
alexander-braun <alexanderbraun@freenet.de>
2020-06-02 17:01:20 +0200

    display size problem with longer numbers solved

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,16 @@ import React, { MouseEvent, useState } from 'react';
 import './style/styles.css';
 import { Display } from './components/Display'
 
-function calc(n1: number, n2: number, operator: String): number {
-  if(operator === '+') return n1 + n2
-  if(operator === '-') return n1 - n2
-  if(operator === '/') return n1 / n2
-  else return n1 * n2
-}
-
-function calculatePercentage(chain: [number, string, number]): number {
-  const op: string = chain[1]
-  const n0: number = chain[0]
-  const n1: number = chain[2]
-
-  if(op[1] === '*' || op[1] === '/') {
-    if(op === '*') {
-      return n0 * n0/100*n1
-    } else {
-      return n0 / n0/100*n1
-    }
-  } else {
-    if(op === '-') {
-      return n0 - (n0 / 100 * n1)
-    } else {
-      return n0 + (n0 / 100 * n1)
-    }
-  }
-}
-
-function getCalcElements(chain: String[]) {
-
-  const positions: number[] = []
-  for(let i = 0; i < chain.length; i++) {
-    chain[i] === '%' && positions.push(i)
-  }
-
-  const elementRanges: number[][] = []
-  for(const element of positions) {
-    elementRanges.push([element - 3, element])
-  }
-
-  const newChain: any[] = []
-  for(let i = 0; i < chain.length; i++) {
-    
-  }
-}
-
 export default function App(): JSX.Element {
 
-  const [chain, updateChain] = useState<String[]>([])
+  const [chain, updateChain] = useState<string[]>([])
   const [result, setResult] = useState<number>(0)
 
   const handleClick = (e: MouseEvent) => {
     const target = e.currentTarget as HTMLButtonElement;
-    const value: String | undefined = target.dataset.value
-    const role: String | undefined = target.dataset.role
-    const lastEl: String = chain[chain.length - 1]
+    const value: string | undefined = target.dataset.value
+    const role: string | undefined = target.dataset.role
+    const lastEl: string = chain[chain.length - 1]
 
     if(value !== undefined) {
       // Case: AC
@@ -70,7 +25,7 @@ export default function App(): JSX.Element {
       // Case: CE
       // Delete last element in chain
       if(value === 'ce') {
-        let chainCopy: String[] = [...chain]
+        let chainCopy: string[] = [...chain]
         chainCopy.pop()
         updateChain(chainCopy)
         return
@@ -83,7 +38,7 @@ export default function App(): JSX.Element {
         // Case: last Element was already an operator
         // Remove last Element and add new value
         if(role === 'operator' && (lastEl === '-' || lastEl === '+' || lastEl === '*' || lastEl === '/')) {
-          const tempChain: String[] = [...chain]
+          const tempChain: string[] = [...chain]
           tempChain.pop()
           updateChain([...tempChain, value])
           return
@@ -95,27 +50,11 @@ export default function App(): JSX.Element {
       // Case: Equals
       // Calculate and update result, reset chain
       if(role === 'result') {
-
-        // Test if percentage sign
-        let hasPercent: Boolean = false
-        for(let i = 0; i < chain.length; i++) {
-          chain[i] === '%' && (hasPercent = true)
-        }
-
-        // Case: There is a percentage sign in the chain
-        if(hasPercent) {
-          let tempResult: number = result
-          let currentOperator: String = ''
-
-
-
-        } else {
-          // eslint-disable-next-line no-eval
-          const tempResult: number = chain.length !== 0 ? eval(chain.join('')) : 0
-          setResult(tempResult)
-          updateChain([tempResult.toString()])
-          return
-        }
+        // eslint-disable-next-line no-eval
+        const tempResult: number = chain.length !== 0 ? eval(chain.join('')) : 0
+        setResult(tempResult)
+        updateChain([tempResult.toString()])
+        return
       }
 
       // Case: Dot
@@ -132,7 +71,6 @@ export default function App(): JSX.Element {
         <Display result={result} chain={chain} />
         <div onClick={handleClick} className="calc-button" id="ac-button" data-role="delete" data-value="ac">AC</div>
         <div onClick={handleClick} className="calc-button" id="ce-button" data-role="delete" data-value="ce">CE</div>
-        <div onClick={handleClick} className="calc-button" id="percentage-button" data-role="operator" data-value="%">%</div>
         <div onClick={handleClick} className="calc-button" id="divide-button" data-role="operator" data-value="/">/</div>
         <div onClick={handleClick} className="calc-button" id="multiply-button" data-role="operator" data-value="*">*</div>
         <div onClick={handleClick} className="calc-button" id="minus-button" data-role="operator" data-value="-">-</div>
