e88cc7e8f567db8cc94ee959e7abc97942f716b1
alexander-braun <alexanderbraun@freenet.de>
2020-06-20 00:29:31 +0200

    Updates

---
+++ precache-manifest.17d15123b9b03c18a895dfec81d86648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05f67e2de8530c981e79e39790036d6",
+    "url": "/neomorphism-calculator/index.html"
+  },
+  {
+    "revision": "69923919642f50121aa7",
+    "url": "/neomorphism-calculator/static/css/main.9d5b9af1.chunk.css"
+  },
+  {
+    "revision": "31d8ffb5e1a6287cb8dd",
+    "url": "/neomorphism-calculator/static/js/2.edacdc8e.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/neomorphism-calculator/static/js/2.edacdc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69923919642f50121aa7",
+    "url": "/neomorphism-calculator/static/js/main.3ab1c424.chunk.js"
+  },
+  {
+    "revision": "ff9361111c336f05338c",
+    "url": "/neomorphism-calculator/static/js/runtime-main.17ae10a1.js"
+  }
+]);
